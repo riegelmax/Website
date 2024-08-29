31f085c46dea65d02c8f87fc3b42bbaa341aa9ac
riegelmax <riegelmax@gmail.com>
2024-08-29 11:47:26 +0200

    "Updated variable name and table name in update_character.php"

--- update_character.php
+++ update_character.php
@@ -1,13 +1,13 @@
 <?php
 include 'connect.php'; // Verbindung zur Datenbank herstellen
 
-$character_id = $_POST['id'];
+$character_ID = $_POST['id'];
 
 // Update the database record
-$db->query("UPDATE characters SET " . implode(", ", array_map(function($column) {
+$db->query("UPDATE charData SET " . implode(", ", array_map(function($column) {
     return "$column = '$_POST[$column]'";
-}, array_keys($_POST))) . " WHERE id = '$character_id'");
+}, array_keys($_POST))) . " WHERE character_ID = '$character_ID'");
 
 // Redirect back to the character details page
-header("Location: character_details.php?id=$character_id");
+header("Location: character_details.php?id=$character_ID");
 exit;
